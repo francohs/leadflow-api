1b2a93dd0c1667efd593fa00fa5af743adeef336
Franco <francohormazabal@gmail.com>
2026-02-13 15:46:23 -0500

    fix prisma error

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
+import dotenv from 'dotenv'
 
-const prisma = new PrismaClient()
+dotenv.config()
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
 
 export default prisma
